5f42e8d5f5d83d0e97d8557b6e93c6bd71f07852
Blake Rhoden <zbrhoden@gmail.com>
2021-09-20 21:42:53 -0500

    nav-bar update

--- src/components/sale-items/sale-items.js
+++ src/components/sale-items/sale-items.js
@@ -4,6 +4,8 @@ import { getAllProducts } from '../ApiManager'
 
 export const Items = () => {
     const [products, setProducts] = useState([])
+    const [cart, setCart] = useState([])
+
     useEffect(
         () => {
                 getAllProducts()
@@ -13,6 +15,32 @@ export const Items = () => {
         },
         []
     )
+
+    function handleAddProductToCart(productId, name, price) {
+        console.log('You clicked add.', productId);
+        let newCart=cart
+        
+        //is this item in the cart
+        //--loop/find
+        //--create a var isFound=true/false
+        //if yes, add 1 to quantity
+        //if no, add item to cart
+        //--use isfound==false
+
+
+        //--create a cart item
+        const cartItem = {
+             id: productId,
+             name,
+             price,
+             quantity: 1
+        } 
+        newCart.push (cartItem)
+        //--push item into cart
+        console.log('contents of newCart', newCart);
+
+        setCart(newCart)
+    }
 
     return (
 
@@ -28,7 +56,11 @@ export const Items = () => {
                                 <ul className="product" >{product.name}</ul>
                                 <ul className="product price">${product.price}
                                 </ul>
-                                <button key={`order--${product.id}`} className="order__button">
+                                <button 
+                                    key={`order--${product.id}`} 
+                                    className="order__button"
+                                    onClick={() => handleAddProductToCart(product.id, product.name, product.price)}
+                                >
                                         Purchase
                                     </button>
                             </ul>
