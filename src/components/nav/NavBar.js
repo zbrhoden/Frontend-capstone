aa070336c90c0cd6f5989d5e86159758c4c38970
Blake Rhoden <zbrhoden@gmail.com>
2021-10-22 15:28:46 -0500

    material ui searchbar

--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -1,13 +1,14 @@
 import "./NavBar.css"
 import Cart from '../cart/Cart'
 import { Link } from "react-router-dom"
+import SearchBar from "@material-ui/core/search-bar";
 
 export const NavBar = (props) => {
 
     return (
         <nav className="NavbarItems">
             <h1 className="NavBar-brand">Blake's Groceries</h1>
-            <div className="menu-item">Contact Us</div>
+            <SearchBar/>
             <div className="menu-item">User Profile</div>
             <div className="navbar__item">
                 <Link className="navbar__link" to="#"
