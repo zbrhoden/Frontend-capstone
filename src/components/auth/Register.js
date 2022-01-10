6020e98ecdb4fd30f1b6af1a233639015b55c6c8
Blake Rhoden <zbrhoden@gmail.com>
2022-01-10 15:20:17 -0600

    auth

--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -1,6 +1,6 @@
 import React, { useRef } from "react"
 import { Link, useHistory} from "react-router-dom"
-import "./Auth.css"
+import "./Login.css"
 
 export const Register = () => {
     const username = useRef()
